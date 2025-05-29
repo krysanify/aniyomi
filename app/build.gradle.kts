9a9a6a0952566f6648f0b267cc3519d18c646d87
Your Name <you@example.com>
2025-05-29 21:26:29 +0800

    upgrade deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,9 +93,10 @@ android {
     splits {
         abi {
             isEnable = true
-            isUniversalApk = true
+            isUniversalApk = false
             reset()
-            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            //noinspection ChromeOsAbiSupport
+            include("arm64-v8a")
         }
     }
 
